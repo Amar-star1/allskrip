29202bafcfca7389421bb2f7be3e7d7caacb042e
Dhika <65761933+Amar-star1@users.noreply.github.com>
2020-08-20 18:02:44 +0700

    Add files via upload

---
+++ ToolsAmar.py
@@ -0,0 +1,40 @@
+import os
+import sys
+
+os.system('clear')
+os.system('figlet Hack FB')
+print "\033[0;35m================================================"
+print "\033[0;32m|| author   :  AmaR                           ||"
+print "\033[0;34m|| facebook :  Dhika Vhanhouthen              ||"
+print "\033[1;33m|| github   :  https://github.com/Amar-star1  ||"
+print "================================================"
+print
+print "\033[1;34m[+] Menu Pilihan [+]"
+print "\033[0;31m[1] Brutal Spam"
+print "\033[1;33m[2] Hack Facebook"
+print "\033[0;31m[3] Spam Lengkap"
+print "\033[1;33m[4] Install Bahan"
+pilih = raw_input('\033[1;0m[?] pilih : ')
+if pilih == "1":
+	os.system('git clone https://github.com/Amar-star1/brutalsms')
+	print "\033[0;35m[~] Penginstallan Selesai"
+	print "[+] ketik \033[0;32m$\033[1;34mcd brutalsms"
+	print "[+] \033[0;35mLalu Ketik \033[0;32m$\033[1;34mpython2 brutal.py"
+elif pilih =="2":
+	os.system('git clone https://github.com/Amar-star1/HackFacebook')
+	print "\033[0;35m[~] Penginstallan Selesai"
+	print "[+] Ketik \033[0;32m$\033[1;34mcd HackFacebook"
+	print "[+] \033[0;35mLalu Ketik \033[0;32m$\033[1;34mpython2 HckFb.py"
+elif pilih =="3":
+	os.system('git clone https://github.com/Amar-star1/spam')
+	print "\033[0;35m[~] Penginstallan Selesai"
+	print "[+] Ketik \033[0;32m$\033[1;34mcd spam"
+	print "[+] \033[0;35mLalu Ketik \033[0;32m$\033[1;34mpython2 KomplitSpam.py"
+elif pilih =="5":
+	os.system('\033[1;0mapt update && apt upgrade')
+	os.system('pkg install python2')
+	os.system('pkg install nano')
+	os.system('pkg install git')
+	os.system('pip2 install requests')
+	os.system('pip2 install mechanize')
+	print "\033[1;0mPenginstallan Selesai"
